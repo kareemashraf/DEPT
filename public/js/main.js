e877cd2fe2fb70ec0d6a1652e6fc911969fe1ce5
Kareem <kareem.ashraf.91@gmail.com>
2019-08-04 03:40:17 +0200

    add sending email api endpoint

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,67 @@
+
 $(document).ready(function() {
 
-	var inputText = $('.movie-title').val();
+	loader();
+	sendEmail();
+
+
+
+
+
+function loader(){
 
 	$( ".go" ).click(function() {
-		$( ".buffer" ).removeClass( "loader" );
+		var email = $(".movie-title").val();
+
+		if (email.length >1) {
+			$( ".buffer" ).removeClass( "loader" );	
+		}
+		
 	});
+
+}
+
+function sendEmail(){
+
+
+	$( ".sendEmail" ).click(function() {
+
+	var email = $("#email-req").val();
+	var title = $("#title-req").val(); 
+	
+	if (email.length >1 && title.length >1) {
+		$('.notification').html('<i style="color: blue;">Email is being sent!</i>');
+	
+		$.ajax({
+            url:"/api/send/"+email+"/movie/"+title,
+            type: "GET",
+            async: true,
+            success: function (data)
+            {
+                console.log(data);
+
+                if (data == 200) {
+                	$('.notification').html('<i style="color: green;">Email has been sent</i>');
+                }else{
+                	$('.notification').html('<i style="color: red;">Email has been Failed!</i>');
+                }
+                
+            },
+            error: function(xhr, textStatus, errorThrown){
+                console.log('request failed');
+            }
+        });
+
+	}
+		
+
+	});
+}
+
+
+
+
+
 });
+
+
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -7,14 +8,15 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpClient\HttpClient;
+use Aws\Ses\SesClient;
 use Vimeo\Vimeo;
 
 class ApiController extends AbstractController
 {
-	private $tmdb;
-	private $vimeoClientId;
-	private $vimeoClientSecret;
-	private $vimeoAccessToken;
+    private $tmdb;
+    private $vimeoClientId;
+    private $vimeoClientSecret;
+    private $vimeoAccessToken;
 
     public function __construct($tmdb, $vimeoClientId, $vimeoClientSecret, $vimeoAccessToken)
     {
@@ -24,58 +26,113 @@ class ApiController extends AbstractController
         $this->vimeoAccessToken = $vimeoAccessToken;
     }
 
-	/**
-      * @Route("/api/trailer/{movie}", methods={"GET"})
-      *
+    /**
+     * @Route("/api/trailer/{movie}", methods={"GET"})
      */
     public function imdb($movie)
     {
-    	$cache = new FilesystemAdapter();
-    	$data = $cache->getItem($movie);
+        $cache = new FilesystemAdapter();
+        $data = $cache->getItem($movie);
 
-    	if (!$data->isHit()) {
-		    // ... item does not exist in the cache
-    		$httpClient = HttpClient::create();
-			$response = $httpClient->request('GET', $this->tmdb.$movie);
-			$contentArr = $response->toArray();
+        if (!$data->isHit()) {
+            // ... item does not exist in the cache
+            $httpClient = HttpClient::create();
+            $response = $httpClient->request('GET', $this->tmdb.$movie);
+            $contentArr = $response->toArray();
 
-			$json = $this->addVimeoTrailers($movie, $contentArr);
+            $json = $this->addVimeoTrailers($movie, $contentArr);
 
-			$data->set($json);
-			$cache->save($data);
+            $data->set($json);
+            $cache->save($data);
+        }
+        // retrieve the value stored by the cache
+        $json = $data->get();
 
-		}
-		// retrieve the value stored by the cache
-		$json = $data->get();
-
-		return new JsonResponse($json);
+        return new JsonResponse($json);
     }
-
 
     protected function addVimeoTrailers($movie, $contentArr)
     {
+        $client = new Vimeo($this->vimeoClientId, $this->vimeoClientSecret, $this->vimeoAccessToken);
 
-    	$client = new Vimeo($this->vimeoClientId, $this->vimeoClientSecret, $this->vimeoAccessToken);
+        foreach ($contentArr['results'] as $key => $content) {
+            $releasedate = explode('-', $content['release_date']);
+            $releaseYear = reset($releasedate);
+            $response = $client->request('/videos', array('query' => $content['title'], 'per_page' => 1, 'sort' => 'relevant'), 'GET');
 
-		foreach ($contentArr['results'] as $key => $content) {
-			$releasedate = explode("-", $content['release_date']);
-			$releaseYear = reset($releasedate);
-			$response = $client->request('/videos', array('query'=>$content['title'], 'per_page'=>1, 'sort'=> 'relevant'), 'GET');
+            try {
+                $videoId = explode('/', reset($response['body']['data'])['uri']);
+                $contentArr['results'][$key]['vimeo']['id'] = end($videoId);
+                $contentArr['results'][$key]['vimeo']['name'] = reset($response['body']['data'])['name'];
+                $contentArr['results'][$key]['vimeo']['duration'] = reset($response['body']['data'])['duration'];
+                $contentArr['results'][$key]['vimeo']['link'] = reset($response['body']['data'])['link'];
+                $contentArr['results'][$key]['vimeo']['description'] = reset($response['body']['data'])['description'];
+                $contentArr['results'][$key]['vimeo']['user'] = reset($response['body']['data'])['user']['name'];
+            } catch (Exception $e) {
+                echo 'Message: '.$e->getMessage();
+            }
+        }
 
-			try{
-				$videoId = explode('/', reset($response['body']['data'])['uri']);
-				$contentArr['results'][$key]['vimeo']['id'] = end($videoId);
-				$contentArr['results'][$key]['vimeo']['name'] = reset($response['body']['data'])['name'];
-				$contentArr['results'][$key]['vimeo']['duration'] = reset($response['body']['data'])['duration'];
-				$contentArr['results'][$key]['vimeo']['link'] = reset($response['body']['data'])['link'];
-				$contentArr['results'][$key]['vimeo']['description'] = reset($response['body']['data'])['description'];
-				$contentArr['results'][$key]['vimeo']['user'] = reset($response['body']['data'])['user']['name'];
-			}
-			catch(Exception $e) {
-			  echo 'Message: ' .$e->getMessage();
-			}	
-		}
-		return $contentArr['results'];
+        return $contentArr['results'];
+    }
 
+    /**
+     * @Route("/api/send/{email}/movie/{movie}", methods={"GET"})
+     */
+    public function send($email, $movie)
+    {
+        $listOfVideos = $this->imdb($movie);
+        $resultsArray = json_decode($listOfVideos->getContent(), true);
+        $html_body = $this->renderView('result.html.twig', ['results' => $resultsArray]);
+
+        $configuration_set = 'DEPT';
+        $subject = 'IMDB about '.$movie;
+        
+        $key = getenv('AWS_KEY'); 
+        $secret = getenv('AWS_SECRET'); 
+
+        // send email using AWS SES
+        $SesClient = new SesClient([
+            'region' => 'eu-west-1',
+            'version' => '2010-12-01', //'latest'
+            'credentials' => [
+                'key' => $key,
+                'secret' => $secret,
+            ],
+        ]);
+
+        try {
+            $result = $SesClient->sendEmail([
+            'Destination' => [
+                'ToAddresses' => [$email],
+            ],
+            'ReplyToAddresses' => [$email],
+            'Source' => 'kareem.ashraf.91@gmail.com',
+            'Message' => [
+                'Body' => [
+                    'Html' => [
+                        'Charset' => 'UTF-8',
+                        'Data' => $html_body,
+                    ],
+                    'Text' => [
+                        'Charset' => 'UTF-8',
+                        'Data' => $listOfVideos,
+                    ],
+                ],
+                'Subject' => [
+                    'Charset' => 'UTF-8',
+                    'Data' => $subject,
+                ],
+            ],
+            'ConfigurationSetName' => $configuration_set,
+        ]);
+        $messageId = $result['MessageId'];
+        $messageStatus = $result['@metadata']['statusCode'];
+
+            return new Response($messageStatus);
+        } catch (AwsException $e) {
+            die($e->getMessage());
+        }
+        //end sending the email
     }
 }
